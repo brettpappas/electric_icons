f8c04e73cf9634f67c60376bfbdcee73c4e69604
Brett Pappas <brett.pappas@gmail.com>
2018-09-02 23:17:03 -0400

    wip

--- lib/electric_icons.dart
+++ lib/electric_icons.dart
@@ -3,14 +3,14 @@ import 'package:flutter/widgets.dart';
 
 class ElectricIcons{
 
-  static IconData v1 = IconData(0xE9AD, fontFamily: 'Electrical', fontPackage: 'electric_icon');
+  static IconData v1 = IconData(0xE9AD, fontFamily: 'Electrical', fontPackage: 'electric_icons');
 
-  static IconData v2 = IconData(0xE907, fontFamily: 'Electrical', fontPackage: 'electric_icon');
+  static IconData v2 = IconData(0xE907, fontFamily: 'Electrical', fontPackage: 'electric_icons');
 
-  static IconData h1 = IconData(0xE98B, fontFamily: 'Electrical', fontPackage: 'electric_icon');
+  static IconData h1 = IconData(0xE98B, fontFamily: 'Electrical', fontPackage: 'electric_icons');
 
-  static IconData h2 = IconData(0xE949, fontFamily: 'Electrical', fontPackage: 'electric_icon');
+  static IconData h2 = IconData(0xE949, fontFamily: 'Electrical', fontPackage: 'electric_icons');
 
-  static IconData icon = IconData(0xE900, fontFamily: 'Electrical', fontPackage: 'electric_icon');
+  static IconData icon = IconData(0xE900, fontFamily: 'Electrical', fontPackage: 'electric_icons');
 
 }
