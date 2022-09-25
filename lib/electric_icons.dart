0d60f1a20ded7fd5a9bfd05c9e4f78960f664cef
Brett Pappas <brett.pappas@gmail.com>
2022-09-24 20:00:16 -0400

    Add null safety

--- lib/electric_icons.dart
+++ lib/electric_icons.dart
@@ -1,16 +1,41 @@
 library electric_icon;
+
 import 'package:flutter/widgets.dart';
 
-class ElectricIcons{
-  static IconData breakers = IconData(0xE901, fontFamily: 'Electrical', fontPackage: 'electric_icons');
+class ElectricIcons {
+  static IconData breakers = IconData(
+    0xE901,
+    fontFamily: 'Electrical',
+    fontPackage: 'electric_icons',
+  );
 
-  static IconData v1 = IconData(0xE9AD, fontFamily: 'Electrical', fontPackage: 'electric_icons');
+  static IconData v1 = IconData(
+    0xE9AD,
+    fontFamily: 'Electrical',
+    fontPackage: 'electric_icons',
+  );
 
-  static IconData v2 = IconData(0xE907, fontFamily: 'Electrical', fontPackage: 'electric_icons');
+  static IconData v2 = IconData(
+    0xE907,
+    fontFamily: 'Electrical',
+    fontPackage: 'electric_icons',
+  );
 
-  static IconData h1 = IconData(0xE98B, fontFamily: 'Electrical', fontPackage: 'electric_icons');
+  static IconData h1 = IconData(
+    0xE98B,
+    fontFamily: 'Electrical',
+    fontPackage: 'electric_icons',
+  );
 
-  static IconData h2 = IconData(0xE949, fontFamily: 'Electrical', fontPackage: 'electric_icons');
+  static IconData h2 = IconData(
+    0xE949,
+    fontFamily: 'Electrical',
+    fontPackage: 'electric_icons',
+  );
 
-  static IconData icon = IconData(0xE900, fontFamily: 'Electrical', fontPackage: 'electric_icons');
+  static IconData icon = IconData(
+    0xE900,
+    fontFamily: 'Electrical',
+    fontPackage: 'electric_icons',
+  );
 }
