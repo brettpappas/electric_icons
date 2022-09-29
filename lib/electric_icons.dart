25aa8d922e9c8d60f078a9b54655c9538bb874c4
Brett Pappas <brett.pappas@gmail.com>
2022-09-29 10:45:28 -0400

    adding const for tree shaking

--- lib/electric_icons.dart
+++ lib/electric_icons.dart
@@ -3,37 +3,37 @@ library electric_icon;
 import 'package:flutter/widgets.dart';
 
 class ElectricIcons {
-  static IconData breakers = IconData(
+  static const IconData breakers = IconData(
     0xE901,
     fontFamily: 'Electrical',
     fontPackage: 'electric_icons',
   );
 
-  static IconData v1 = IconData(
+  static const IconData v1 = IconData(
     0xE9AD,
     fontFamily: 'Electrical',
     fontPackage: 'electric_icons',
   );
 
-  static IconData v2 = IconData(
+  static const IconData v2 = IconData(
     0xE907,
     fontFamily: 'Electrical',
     fontPackage: 'electric_icons',
   );
 
-  static IconData h1 = IconData(
+  static const IconData h1 = IconData(
     0xE98B,
     fontFamily: 'Electrical',
     fontPackage: 'electric_icons',
   );
 
-  static IconData h2 = IconData(
+  static const IconData h2 = IconData(
     0xE949,
     fontFamily: 'Electrical',
     fontPackage: 'electric_icons',
   );
 
-  static IconData icon = IconData(
+  static const IconData icon = IconData(
     0xE900,
     fontFamily: 'Electrical',
     fontPackage: 'electric_icons',
